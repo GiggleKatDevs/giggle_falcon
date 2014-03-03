2d3ac853def65cf51cc21f16d801456f6414e895
Sean Hoyt <seanhoyt963@gmail.com>
2014-03-03 09:30:40 -0600

    Add OTA ID

--- device_falcon.mk
+++ device_falcon.mk
@@ -37,6 +37,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.radio.mode_pref_nv10=1 \
 	persist.radio.no_wait_for_card=1 \
 	persist.radio.dfr_mode_set=1
+	
+# OTA ID
+PRODUCT_PROPERTY_OVERRIDES += \
+    otaupdater.otaid=liquidfalcon
 
 $(call inherit-product, device/motorola/msm8226-common/keylayout/keylayout.mk)
 $(call inherit-product, vendor/motorola/falcon/falcon-vendor.mk)
