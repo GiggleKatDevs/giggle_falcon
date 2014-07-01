7f69dfa0a7f915c040b54b9496b283197ce8ea81
CPA-Poke <brock.shahan@cox.net>
2014-07-01 10:22:46 -0400

    Make ROM zip compatible with ext4/f2fs
    Requires "f2fs2" branch of LiquidSmooth/android_build
    
    Change-Id: I81674f0b723886441580af3391705d8eb742efd0

--- device.mk
+++ device.mk
@@ -25,6 +25,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     gpe-fstab.qcom
 
+PRODUCT_COPY_FILES += \
+    device/motorola/falcon/format-system.sh:system/extras/format-system.sh
+
+
 # Wifi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
